3403a8803702aaf77780ca97391177abb9861349
Chayon <chayon15-3633@diu.edu.bd>
2022-11-30 03:03:37 +0600

    third commit

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ async function run() {
             console.log(query)
             res.send(query)
         })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            console.log('email', email)
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log('user', user)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' })
+        })
     }
 
     finally {
