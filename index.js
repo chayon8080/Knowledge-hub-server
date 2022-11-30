47c00ada298abad14b45255c2925040f473dfe27
Chayon <chayon15-3633@diu.edu.bd>
2022-11-30 23:15:57 +0600

    tenth commit

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
             console.log(query)
             res.send(query)
         })
-        app.get('/bookings', verifyJWT, async (req, res) => {
+        app.get('/bookings', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
