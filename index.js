0640895d0c4fc143f5b172931cdc3a9e14436c7f
Chayon <chayon15-3633@diu.edu.bd>
2022-11-30 18:57:41 +0600

    fifth commit

--- index.js
+++ index.js
@@ -38,7 +38,11 @@ async function run() {
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
         })
-
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result)
+        })
 
 
 
