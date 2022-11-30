792684607740e994cc6907b5dd63d077a2a9e4ce
Chayon <chayon15-3633@diu.edu.bd>
2022-11-30 22:16:53 +0600

    seventh commit

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wd5n7ku.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized access')
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        console.log(err, decoded)
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+
 async function run() {
     try {
         const catagoriesCollection = client.db('knowledgeHub').collection('catagories')
